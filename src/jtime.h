2ad197cacccab4a2703d3f77d9535ab58d85f9d7
Jlobblet <jlobblet-github@gmx.co.uk>
2021-12-02 23:31:16 +0000

    More stuff in jtime

--- src/jtime.h
+++ src/jtime.h
@@ -3,6 +3,9 @@
 
 #include <time.h>
 
+clock_t elapsed_ns(clock_t start, clock_t end);
 clock_t elapsed_us(clock_t start, clock_t end);
+clock_t elapsed_ms(clock_t start, clock_t end);
+clock_t elapsed_s(clock_t start, clock_t end);
 
 #endif //JC_JTIME_H
