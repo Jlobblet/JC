0d9c6c60cb4fc289af577638fdc69d81ed0642c2
Jlobblet <jlobblet-github@gmx.co.uk>
2021-12-02 20:34:33 +0000

    Document jio.c

--- src/jio.c
+++ src/jio.c
@@ -1,8 +1,30 @@
+/// \file
+/// Utility functions for dealing with input/output.
 #include <errno.h>
 #include <malloc.h>
 #include <stdio.h>
 #include "jio.h"
 
+/// Read all lines from a file into an array of null-terminal strings.
+///
+/// The read lines include newline character(s), and are null terminated.
+/// \param filepath Name of the file to open.
+/// \param lines Pointer to an array to store read lines in.
+/// \param lengths Pointer to an array to store the length of read lines in.
+/// \param n The length of the `lines` and `lengths` arrays, which must be equal.
+/// If `n` is `0`, `lines` and `lengths` will be allocated memory to begin with.
+/// \return On success, returns the number of lines read (and hence the length of `lines` and `lengths`).
+/// On failure, returns `-1` and `errno` is set to indicate the error.
+/// \remark Lines and lengths should either be large enough to fit the entire file or able to be
+/// dynamically reallocated.
+/// \remark This function allocates a `char*` array for each line in the file. The programmer must
+/// free each entry in the `lines` array, then `lines` and `lengths`, when freeing allocated memory.
+/// \remark All allocations made are shrunk to the minimum size after reading in the file.
+/// \subsubsection Errors
+/// `errno`  | Description
+/// -------- | -----------
+/// `EINVAL` | `lines` or `lengths` is a null pointer.
+/// `ENOMEM` | There was insufficient memory to expand either `lines` or `lengths`.
 uptr read_all_lines(const char* filepath, char*** lines, uptr** lengths, uptr n) {
     if (lines == NULL) {
         errno = EINVAL;
