71f911e5b1ca8d614349e0d792e54db9f6458f06
John Blundell <jlobblet-github@gmx.co.uk>
2021-12-05 02:31:14 +0000

    jbits progress

--- src/jbits.c
+++ src/jbits.c
@@ -1,17 +1,70 @@
+/// \file
+/// Bit manipulation
+
 #include <assert.h>
 #include <stdlib.h>
 #include "jbits.h"
 
+#define popcount_body \
+uptr sum = 0;         \
+while(x) {            \
+    sum += sum & 1U;  \
+    x >>= 1;          \
+}                     \
+return sum;
+
+#define pop0count_body \
+x = ~x;                \
+popcount_body
+
+uptr popcount8(u8 x) {
+    popcount_body
+}
+
+uptr popcount16(u16 x) {
+    popcount_body
+}
+
+uptr popcount32(u32 x) {
+    popcount_body
+}
+
+uptr popcount64(u64 x) {
+    popcount_body
+}
+
+uptr pop0count8(u8 x) {
+    pop0count_body
+}
+
+uptr pop0count16(u16 x) {
+    pop0count_body
+}
+
+uptr pop0count32(u32 x) {
+    pop0count_body
+}
+
+uptr pop0count64(u64 x) {
+    pop0count_body
+}
+
+/// Initialise a `BitArray2d` struct.
+///
+/// This function allocates two arrays - one to store `row_starts`, which contains
+/// pointers to the start of each row, and `data`, which contains each row.
+/// \param arr The array to initialise.
+/// \return On success, `0`. On error, `-1`, and `errno` is set to indicate the error.
 iptr BitArray2d_init(BitArray2d* arr) {
     assert(arr->rows > 0);
     assert(arr->cols > 0);
-    arr->row_starts = calloc(arr->rows, sizeof(uptr*));
+    arr->row_starts = calloc(arr->rows, sizeof(BitArray2dBacker*));
     if (arr->row_starts == NULL) {
         return -1;
     }
 
     uptr row_width = get_row_width(arr);
-    arr->data = calloc(row_width, sizeof(uptr));
+    arr->data = calloc(row_width, sizeof(BitArray2dBacker));
     if (arr->data == NULL) {
         return -1;
     }
@@ -23,18 +76,33 @@ iptr BitArray2d_init(BitArray2d* arr) {
     return 0;
 }
 
+/// Handle cleaning up a `BitArray2d`.
+///
+/// \param arr The `BitArray2d` to clean up (by pointer).
 void BitArray2d_dest(BitArray2d* arr) {
     free(arr->row_starts);
     free(arr->data);
 }
 
-uptr BitArray2d_get(BitArray2d* arr, uptr row, uptr col) {
+/// Get the value in a given cell of a `BitArray2d`.
+///
+/// \param arr The `BitArray2d` to retrieve a bit from.
+/// \param row The row index (0-based) to retrieve the bit from.
+/// \param col The column index (0-based) to retrieve the bit from.
+/// \return `0` or `1`.
+BitArray2dBacker BitArray2d_get(BitArray2d* arr, uptr row, uptr col) {
     assert(row < arr->rows);
     assert(col < arr->cols);
     uptr row_width = get_row_width(arr);
     return (arr->row_starts[row][col / row_width] >> get_offset(col)) & 1U;
 }
 
+/// Set the value in a given cell of a `BitArray2d`.
+///
+/// \param arr The `BitArray2d` to set a bit in.
+/// \param row The row index (0-based) of the bit to set.
+/// \param col The column index (0-based) of the bit to set.
+/// \param value The value to set at the given coordinates, `0` or `1`.
 void BitArray2d_set(BitArray2d* arr, uptr row, uptr col, uptr value) {
     assert(row < arr->rows);
     assert(col < arr->cols);
@@ -45,20 +113,90 @@ void BitArray2d_set(BitArray2d* arr, uptr row, uptr col, uptr value) {
             | (value << get_offset(col));
 }
 
+/// Set the value in a given cell of a `BitArray2d` to `1`.
+/// \param arr The `BitArray2d` to set a bit in.
+/// \param row The row index (0-based) of the bit to set.
+/// \param col The column index (0-based) of the bit to set.
 void BitArray2d_on(BitArray2d* arr, uptr row, uptr col) {
     assert(row < arr->rows);
     assert(col < arr->cols);
     arr->row_starts[row][col / get_row_width(arr)] |= 1U << get_offset(col);
 }
 
+/// Set the value in a given cell of a `BitArray2d` to `0`.
+/// \param arr The `BitArray2d` to set a bit in.
+/// \param row The row index (0-based) of the bit to set.
+/// \param col The column index (0-based) of the bit to set.
 void BitArray2d_off(BitArray2d* arr, uptr row, uptr col) {
     assert(row < arr->rows);
     assert(col < arr->cols);
     arr->row_starts[row][col / get_row_width(arr)] &= ~(1U << get_offset(col));
 }
 
+/// Toggle value in a given cell of a `BitArray2d`.
+///
+/// Cells with a `0` in them will be set to `1`, and cells with a `1` in them
+/// will be set to `0`.
+/// \param arr The `BitArray2d` to set a bit in.
+/// \param row The row index (0-based) of the bit to set.
+/// \param col The column index (0-based) of the bit to set.
 void BitArray2d_toggle(BitArray2d *arr, uptr row, uptr col) {
     assert(row < arr->rows);
     assert(col < arr->cols);
     arr->row_starts[row][col / get_row_width(arr)] ^= 1U << get_offset(col);
+}
+
+uptr BitArray2d_count_on(BitArray2d* arr) {
+    uptr sum = 0;
+    for (uptr i = 0; i < arr->rows; i++) {
+        for (uptr j = 0; j < get_row_width(arr)
+#ifdef NDEBUG
+        - 1
+#endif //NDEBUG
+        ; j++) {
+            sum += popcount(arr->row_starts[i][j]);
+        }
+#ifdef NDEBUG
+        for (uptr c = BITS * (arr->cols / BITS); c < arr->cols; c++) {
+            sum += BitArray2d_get(arr, i, c);
+        }
+#endif // NDEBUG
+    }
+    return sum;
+}
+
+uptr BitArray2d_count_off(BitArray2d* arr) {
+    uptr sum = 0;
+    for (uptr i = 0; i < arr->rows; i++) {
+        for (uptr j = 0; j < get_row_width(arr)
+#ifndef NDEBUG
+        - 1
+#endif // NDEBUG
+        ; j++) {
+            sum += pop0count(arr->row_starts[i][j]);
+        }
+#ifdef NDEBUG
+        for (uptr c = BITS * (arr->cols / BITS); c < arr->cols; c++) {
+            sum += BitArray2d_get(arr, i, c);
+        }
+#endif
+    }
+    return sum;
+}
+
+void BitArray2d_flip(BitArray2d* arr) {
+    for (uptr i = 0; i < arr->rows; i++) {
+        for (uptr j = 0; j < get_row_width(arr)
+#ifndef NDEBUG
+        - 1
+#endif // NDEBUG
+        ; j++) {
+            arr->row_starts[i][j] = ~arr->row_starts[i][j];
+        }
+#ifndef NDEBUG
+        for (uptr c = BITS * (arr->cols / BITS); c < arr->cols; c++) {
+            BitArray2d_off(arr, i, c);
+        }
+#endif // NDEBUG
+    }
 }
--- src/jbits.h
+++ src/jbits.h
@@ -3,15 +3,17 @@
 
 #include "jint.h"
 
+typedef uptr BitArray2dBacker;
+
 typedef struct BitArray2d {
     uptr rows, cols;
     uptr **row_starts;
-    uptr *data;
+    BitArray2dBacker *data;
 } BitArray2d;
 
-static const uptr BITS = (sizeof(uptr) * 8);
+static const uptr BITS = (sizeof(BitArray2dBacker) * 8);
 
-static inline uptr get_row_width(BitArray2d* arr) {
+static inline uptr get_row_width(BitArray2d *arr) {
     return (arr->cols + BITS - 1) / BITS;
 }
 
@@ -19,18 +21,36 @@ static inline uptr get_offset(uptr col) {
     return col % BITS;
 }
 
+uptr popcount8(u8 x);
+uptr popcount16(u16 x);
+uptr popcount32(u32 x);
+uptr popcount64(u64 x);
+
+#define popcount(x) _Generic((x),      \
+u8 : popcount8,                        \
+u16: popcount16,                       \
+u32: popcount32,                       \
+u64: popcount64)                       \
+(x)
+
+uptr pop0count8(u8 x);
+uptr pop0count16(u16 x);
+uptr pop0count32(u32 x);
+uptr pop0count64(u64 x);
+
+#define pop0count(x) _Generic((x),      \
+u8 : pop0count8,                        \
+u16: pop0count16,                       \
+u32: pop0count32,                       \
+u64: pop0count64)                       \
+(x)
+
 iptr BitArray2d_init(BitArray2d *arr);
-
 void BitArray2d_dest(BitArray2d *arr);
-
 uptr BitArray2d_get(BitArray2d *arr, uptr row, uptr col);
-
-void BitArray2d_set(BitArray2d *arr, uptr row, uptr col, uptr value);
-
+void BitArray2d_set(BitArray2d *arr, uptr row, uptr col, BitArray2dBacker value);
 void BitArray2d_on(BitArray2d *arr, uptr row, uptr col);
-
 void BitArray2d_off(BitArray2d *arr, uptr row, uptr col);
-
 void BitArray2d_toggle(BitArray2d *arr, uptr row, uptr col);
 
 #endif //JC_JBITS_H
