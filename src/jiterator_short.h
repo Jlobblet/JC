bbb3ed32cdf1b33bda08dd907ee9f92bbe115d27
Jlobblet <jlobblet-github@gmx.co.uk>
2022-05-26 21:11:55 +0100

    Update jiterator_short.h

--- src/jiterator_short.h
+++ src/jiterator_short.h
@@ -4,11 +4,24 @@
 #include "jiterator.h"
 
 #define collect jiterator_collect
+
 #define vector_reset jiterator_vector_reset
 #define vector jiterator_vector_init
+
 #define map jiterator_map_init
 #define action jiterator_action_init
 #define filter jiterator_filter_init
+
+#define reduce jiterator_reduce
+#define fold jiterator_fold
+#define scan jiterator_scan_init
+
+#define min_max jiterator_min_max
+#define min jiterator_min
+#define max jiterator_max
+#define all jiterator_all
+#define any jiterator_any
+
 #define iota jiterator_iota_init
 #define take jiterator_take_init
 #define drop jiterator_drop_init
